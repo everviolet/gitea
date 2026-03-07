9db1248a8f754aad95d813cd9e346eea7ae2c65a
june <me@koi.rip>
2026-03-07 19:40:41 +0100

    build: evergardenify

--- build.ts
+++ build.ts
@@ -2,28 +2,54 @@
 
 import * as path from "@std/path";
 import * as sass from "sass";
-import { flavorEntries, flavors } from "@catppuccin/palette";
-import { updateReadme } from "@catppuccin/deno-lib";
+import palette from "palette.json" with { type: "json" };
 
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
-const accents = flavors.mocha.colorEntries
+const accents = Object.entries(palette.winter.colors)
   .filter(([_, { accent }]) => accent)
   .map(([accentName]) => accentName);
 
 Deno.mkdirSync(path.join(__dirname, "dist"), { recursive: true });
 
-const sassBuilder = (flavor: string, accent: string) => `
-@import "@catppuccin/palette/scss/${flavor}";
+const sassBuilder = (variant: string, accent: string) => `
+@import "@everviolet/palette/scss/${variant}.scss";
 $accent: $${accent};
-$isDark: ${flavor !== "latte"};
+$isDark: ${variant !== "summer"};
 @import "theme";
 `;
 
-flavorEntries.forEach(([flavorName, flavor]) => {
-  flavor.colorEntries
+const renameColors = (c) => {
+  return c
+    .replace(/peach/g, 'orange')
+    .replace(/lavender/g, 'snow')
+    .replace(/mauve/g, 'purple')
+    .replace(/flamingo/g, 'cherry')
+    .replace(/rosewater/g, 'cherry')
+    .replace(/sky/g, 'skye')
+    .replace(/maroon/g, 'red')
+    .replace(/teal/g, 'aqua');
+};
+
+const srcDir = path.join(__dirname, "src");
+for await (const dirEntry of Deno.readDir(srcDir)) {
+  if (dirEntry.isFile) {
+    const filePath = path.join(srcDir, dirEntry.name);
+    const content = await Deno.readTextFile(filePath);
+    const newContent = renameColors(content);
+    await Deno.writeTextFile(filePath, newContent);
+  }
+}
+
+Object.entries(palette).forEach(([variantName, variant]) => {
+  if (!variant.colors || Object.keys(variant.colors).length === 0) {
+    console.warn(`no colors in ${variantName}`);
+    return;
+  }
+
+  Object.entries(variant.colors)
     .filter(([_, { accent }]) => accent)
     .forEach(([accentName]) => {
-      const input = sassBuilder(flavorName, accentName);
+      const input = sassBuilder(variantName, accentName);
       const result = sass.compileString(input, {
         loadPaths: [
           path.join(__dirname, "src"),
@@ -35,42 +61,15 @@ flavorEntries.forEach(([flavorName, flavor]) => {
         path.join(
           __dirname,
           "dist",
-          `theme-catppuccin-${flavorName}-${accentName}.css`,
+          `theme-evergarden-${variantName}-${accentName}.css`,
         ),
         result.css,
       );
 
       Deno.writeTextFileSync(
-        path.join(__dirname, "dist", `theme-catppuccin-${accentName}-auto.css`),
-        `@import "./theme-catppuccin-latte-${accentName}.css" (prefers-color-scheme: light);
-@import "./theme-catppuccin-mocha-${accentName}.css" (prefers-color-scheme: dark);`,
+        path.join(__dirname, "dist", `theme-evergarden-${accentName}-auto.css`),
+        `@import "./theme-evergarden-summer-${accentName}.css" (prefers-color-scheme: light);
+@import "./theme-evergarden-winter-${accentName}.css" (prefers-color-scheme: dark);`,
       );
     });
 });
-
-const flavorAccentIni = `
-\`\`\`ini
-[ui]
-THEMES = ${
-  flavorEntries
-    .map(([f]) => accents.map((a) => `catppuccin-${f}-${a}`).join(","))
-    .join(",")
-}
-\`\`\`
-`;
-
-const themeAutoIni = `
-\`\`\`ini
-[ui]
-THEMES = ${accents.map((a) => `catppuccin-${a}-auto`).join(",")}
-\`\`\`
-`;
-
-const oldReadme = Deno.readTextFileSync(path.join(__dirname, "README.md"));
-let newReadme = updateReadme(oldReadme, flavorAccentIni, {
-  section: "ini",
-});
-newReadme = updateReadme(newReadme, themeAutoIni, {
-  section: "ini-auto",
-});
-Deno.writeTextFileSync(path.join(__dirname, "README.md"), newReadme);
